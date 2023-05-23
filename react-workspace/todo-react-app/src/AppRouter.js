6c78723f3daa62508e488afaa3a4d8f54bb82f5e
developerjini <i5veagl@gmail.com>
2023-05-23 10:50:44 +0900

    로그인 컴포넌트

---
+++ react-workspace/todo-react-app/src/AppRouter.js
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./Login";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright &copy"}
+      fsoftwareengineer, {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const AppRouter = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </div>
+  );
+};
+
+export default AppRouter;
